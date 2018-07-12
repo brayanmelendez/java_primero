8c356e5804929368e013f46282b4527192ca7b90
brayanmelendez <brayanmelendezarnedo@gmail.com>
2018-07-11 19:41:13 -0500

    suma de numeros

---
+++ src/primero/test2.java
@@ -0,0 +1,20 @@
+package primero;
+
+public class test2 {
+
+    public static void main(String[] args) {
+
+        int c = 2 + 2;
+
+        int b = c * 0;
+
+        int x = b + c * 0;
+        System.out.println(c + "\n" + b + "\n" + x + "\n" + suma(1,35));
+
+    }
+
+    private static int suma(int a, int b) {
+        return a + b;
+    }
+
+}
