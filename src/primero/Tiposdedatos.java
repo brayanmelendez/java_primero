44302473e674a816ade521cac0cda9495238bdf5
brayanmelendez <brayanmelendezarnedo@gmail.com>
2018-07-12 13:48:09 -0500

    Tipos de datos

---
+++ src/primero/Tiposdedatos.java
@@ -0,0 +1,31 @@
+package primero;
+
+public class Tiposdedatos {
+
+    public static void main(String[] args) {
+
+        int v1; // para almacenar datos de tipo munerico
+
+        v1 = 2; // aqui es donde le vamos valos a nuetra variable
+
+        System.out.println(v1);
+
+        Integer v2; // para almacenar dator de tipo numerico
+
+        v2 = 3;
+        System.out.println(v2);
+        
+        double v3; // para valores con decimales
+        
+        v3 = 3.5;
+        
+        System.out.println(v3);
+        
+        String cadena;
+        
+        cadena = "hola gente";
+        
+        System.out.println(cadena);
+        
+    }
+}
