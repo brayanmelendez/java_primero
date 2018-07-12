d40198525aeee7441ce2cc9b8e6e0e02369f90d9
brayanmelendez <brayanmelendezarnedo@gmail.com>
2018-07-11 19:18:37 -0500

    mi primer commit

---
+++ src/primero/test.java
@@ -0,0 +1,14 @@
+package primero;
+
+public class test {
+
+    public static void main(String[] args) {
+        test c11 = new test();
+        c11.metodo1();
+    }
+
+    public void metodo1() {
+        System.out.print("Hola Mundo");
+    }
+
+}
