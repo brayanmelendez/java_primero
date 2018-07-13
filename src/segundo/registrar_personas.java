259507649f029ad6986a3018056c9d186e87f9e3
brayanmelendez <brayanmelendezarnedo@gmail.com>
2018-07-12 20:07:42 -0500

    secuencia de escape

--- src/segundo/registrar_personas.java
+++ src/segundo/registrar_personas.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package segundo;
 import segundo.persona;
-/**
- *
- * @author braya
- */
+
 public class registrar_personas {
     
     public static void main(String[] args) {
---
+++ src/tercero/charysecuenciadeescape.java
@@ -0,0 +1,21 @@
+package tercero;
+
+public class charysecuenciadeescape {
+
+    public static void main(String[] args) {
+        
+        char car; // en esta variable solo se puede guardar una expresion de tipo string
+        
+        car = 'b';
+        
+        System.out.println(car);
+        
+        System.out.println("hola mundo \n alien"); // esto es para un salto de linea 
+        
+        System.out.println("hola mundo \tde nuevo"); // es una tabulacion en el mensaje de salida
+        
+    }
+
+
+    
+}
